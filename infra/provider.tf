d5551e00fdbae899e63c76cb6a6269ae2f3b14d3
lordsesay <lordsesay@outlook.com>
2026-05-13 16:58:37 -0400

    fix: add S3 backend for shared Terraform state between EC2 and Jenkins

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "s3" {
+    bucket = "lordsesay-fullstack-automation-artifacts-demo"
+    key    = "terraform/state/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
