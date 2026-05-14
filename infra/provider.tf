6536920faada105220566f0904dd1fd17f39787e
lordsesay <lordsesay@outlook.com>
2026-05-14 13:49:39 -0400

    feat: add infrastructure pipeline with state locking and manual approval

--- infra/provider.tf
+++ infra/provider.tf
@@ -2,9 +2,11 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket = "lordsesay-fullstack-automation-artifacts-demo"
-    key    = "terraform/state/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "lordsesay-fullstack-automation-artifacts-demo"
+    key            = "terraform/state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "fullstack-automation-tf-lock"
+    encrypt        = true
   }
 
   required_providers {
